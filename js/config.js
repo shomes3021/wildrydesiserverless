d9febf76bafdbd5c6f224bd69353468a840f36c0
shomeshwar-aws <gosoftg@gmail.com>
2024-06-28 17:04:07 +0530

    Initial Commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_fMkul0Fbi', 
+        userPoolClientId: '539dfgrgg7onkeevjvisjb3v47', 
+        region: 'ap-south-1' 
+    },
+    api: {
+        invokeUrl: 'https://b8hr1whxef.execute-api.ap-south-1.amazonaws.com/prod',
+    }
+};
